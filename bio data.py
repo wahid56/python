2f6568ee6e13642df50613cd995dc26fa0ef28c1
wahid56 <120100198+wahid56@users.noreply.github.com>
2022-12-24 12:54:08 +0500

    Add files via upload

---
+++ bio data.py
@@ -0,0 +1,36 @@
+
+Name="abdulwahid"
+FatherName="abdulqadir"
+Age=18.5
+Class="year1"
+Program="computer science"
+Universty="theroyalcolosseum"
+Height=5.5
+presentinclass= True
+
+
+
+print("========================")
+print("     Bio Data           ")
+print("========================")
+
+
+
+
+print("Name:",Name)
+print("FatherName:",FatherName)
+print("Age:",Age)
+print("Class:",Class)
+print("Program:",Program)
+print("Universty:",Universty)
+print("Height",Height)
+print("presentinclass",presentinclass)
+
+print(type(Name))
+print(type(FatherName))
+print(type(Age))
+print(type(Class))
+print(type(Program))
+print(type(Universty))
+print(type(Height))
+print(type(presentinclass))
